3ed2c963f4594b2a8b975cdeac94d9d81ffeb14c
Alex Hong <hongalex@usc.edu>
2016-02-27 03:03:11 -0800

    created xcode project

---
+++ p!ng/p!ng/DataManager.swift
@@ -0,0 +1,10 @@
+//
+//  DataManager.swift
+//  p!ng
+//
+//  Created by Alex Hong on 2/27/16.
+//  Copyright © 2016 yakk. All rights reserved.
+//
+
+import Foundation
+
---
+++ p!ng/p!ngUITests/p_ngUITests.swift
@@ -0,0 +1,36 @@
+//
+//  p_ngUITests.swift
+//  p!ngUITests
+//
+//  Created by Alex Hong on 2/27/16.
+//  Copyright © 2016 yakk. All rights reserved.
+//
+
+import XCTest
+
+class p_ngUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
